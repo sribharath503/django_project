16e0096a62cecba7a2eb5bf713ff57f20e959542
sribharath503 <sribharathq@gmail.com>
2025-07-24 09:19:35 +0530

    ordermanagement app customer model CRUD views created successfully

--- OrderManagement/urls.py
+++ OrderManagement/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns=[
     path('add_customers/', add_customers),
     path('view_customers/', view_customers),
+    path('delete_customers/<int:id>/', delete_customers,name="delete_customers"),
+    path('update_customers/<int:id>/', update_customers,name="update_customers"),
 ]
--- OrderManagement/views.py
+++ OrderManagement/views.py
@@ -23,3 +23,26 @@ def view_customers(request):
        'all_customers': selected_customers
    }
    return render(request,'customers.html',context)
+
+def delete_customers(request,id):
+    selected_customer=customers.objects.get(id=id)
+    selected_customer.delete()
+    return redirect('/order_management/view_customers/')
+
+def update_customers(request, id):
+    selected_customer=customers.objects.get(id=id)
+    
+    if request.method == 'POST':
+        form=customer_form(request.POST, instance=selected_customer)
+        if form.is_valid():
+            form.save()
+            return redirect('/order_management/view_customers/')
+    else:
+        form=customer_form(instance=selected_customer)
+
+    context={
+        'customer_form':form
+    }
+
+    return render(request,'add_customers.html',context)
+
