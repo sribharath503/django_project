6247c114cfc9118a390d30d1afb5c1ef35375c32
sribharath503 <sribharathq@gmail.com>
2025-07-25 10:30:44 +0530

    add new external mysql db in my django project

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'DjangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
