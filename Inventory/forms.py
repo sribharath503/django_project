5b38133919191815df0ce16ef9f7d15b0a5d3644
sribharath503 <sribharathq@gmail.com>
2025-07-28 13:27:02 +0530

    adding css style and bootstrap style for the django forms by including widgets and some html changes for styling purpose in add products,add customers,add orders pages

--- Inventory/forms.py
+++ Inventory/forms.py
@@ -1,7 +1,14 @@
-from django.forms import ModelForm
+from django import forms
 from .models import *
 
-class product_form(ModelForm):
+class product_form(forms.ModelForm):
     class Meta:
         model = products
         fields = '__all__'
+
+        widgets={
+            'product_name':forms.TextInput(attrs={'class':'form-control'}),
+            'product_code':forms.TextInput(attrs={'class':'form-control'}),
+            'price':forms.NumberInput(attrs={'class':'form-control'}),
+            'gst':forms.NumberInput(attrs={'class':'form-control'}),
+        }
--- OrderManagement/forms.py
+++ OrderManagement/forms.py
@@ -1,12 +1,30 @@
-from django.forms import ModelForm
+from django import forms
 from .models import *
 
-class customer_form(ModelForm):
+class customer_form(forms.ModelForm):
     class Meta:
         model = customers
         fields = '__all__'
 
-class order_form(ModelForm):
+        widgets={
+            'customer_name':forms.TextInput(attrs={'class':'form-control'}),
+            'customer_since':forms.DateInput(attrs={'class':'form-control'}),
+        }
+
+
+
+class order_form(forms.ModelForm):
     class Meta:
         model= orders
         fields = ['customer','product','order_number','order_date','quantity']
+
+        widgets={
+           'customer':forms.Select(attrs={'class':'form-control'}),
+           'product':forms.Select(attrs={'class':'form-control'}),
+           'order_number':forms.NumberInput(attrs={'class':'form-control'}),
+           'order_date':forms.DateInput(attrs={'class':'form-control'}),
+           'quantity': forms.NumberInput(attrs={'class':'form-control'}),
+           'amount':forms.NumberInput(attrs={'class':'form-control'}),
+           'gst_amount':forms.NumberInput(attrs={'class':'form-control'}),
+           'total_price':forms.NumberInput(attrs={'class':'form-control'}),
+        }
